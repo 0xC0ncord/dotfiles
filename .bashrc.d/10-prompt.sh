9f9d6eb06147749a252a4eb726a9a4d64b075b85
0xC0ncord <concord@fuwafuwatime.moe>
2020-02-11 08:50:10 -0500

    Hack to make prompt work on Bash <4.4, add root's prompt, and improve readability

--- .bashrc.d/10-prompt.sh
+++ .bashrc.d/10-prompt.sh
@@ -4,20 +4,40 @@
 export GIT_DISCOVERY_ACROSS_FILESYSTEM=1
 
 # PS1 generation
-make_ps1() {
+make_PS1() {
     # PS1 itself
-    p="\[\033]0;\u@\h:\w\007\]\[\033[01;32m\]\u@\h\[\033[01;34m\] \w\[\033[01;33m\]\$(git_prompt) \[\033[01;34m\]\\$\[\033[00m\] "
+    if [[ $EUID -eq 0 ]]; then
+        # Root's prompt
+	P="\[\033]0;\h:\w\007\]\[\033[01;31m\]\h\[\033[01;34m\] \w\[\033[01;33m\]\$(git_prompt) \[\033[01;34m\]\\$\[\033[00m\] "
+    else
+        # User prompt
+	P="\[\033]0;\u@\h:\w\007\]\[\033[01;32m\]\u@\h\[\033[01;34m\] \w\[\033[01;33m\]\$(git_prompt) \[\033[01;34m\]\\$\[\033[00m\] "
+    fi
 
     # PS1 but parsed and ANSI sequences removed; for length calculation
-    pc=$(echo "${p@P}" | perl -pe 's/\e([^\[\]]|\[.*?[a-zA-Z]|\].*?\a)//g' | col -b)
+    if [[ ${BASH_VERSION:0:1} -lt 4 ]] || [[ ${BASH_VERSION:2:1} -lt 4 ]]; then
+        # @P operation is unsupported in Bash < 4.4
+        PC=$(echo "${USER}@$(hostname) $(dirs +0)$(git_prompt) $")
+    else
+        PC=$(echo "${P@P}" | perl -pe 's/\e([^\[\]]|\[.*?[a-zA-Z]|\].*?\a)//g' | col -b)
+    fi
 
     # If we don't have much room for typing, use the 2-line prompt
-    if [[ $(tput cols) -le $((${#pc} + 48)) ]]; then
-        ps1="\[\033[01;34m\]╭\[\033[00m\] $(echo $p | sed 's/\ \([0-9;\[\$]*m\\\]\)*$//')\n\[\033[01;34m\]╰╼ \$\[\033[00m\] "
+    if [[ $(tput cols) -le $((${#PC} + 48)) ]]; then
+        PS1="\[\033[01;34m\]╭\[\033[00m\] $(echo ${P} | sed 's/\ \([0-9;\[\$]*m\\\]\)*$//')\n\[\033[01;34m\]╰╼ \$\[\033[00m\] "
     else
-        ps1="$p"
+        PS1="${P}"
     fi
-    echo "${ps1@P}"
+    if [[ ${BASH_VERSION:0:1} -lt 4 ]] || ([[ ${BASH_VERSION:0:1} -eq 4 ]] && [[ ${BASH_VERSION:2:1} -lt 4 ]]); then
+        # @P operation is unsupported in Bash < 4.4
+        # Nasty hack to parse the prompt - slow!
+        # FIXME: breaks when some special characters are used in commands (i.e. '!')
+        export PS1="${PS1}"
+        export -f git_prompt
+        echo "$(bash --rcfile <(echo "PS1='$PS1'") -i <<<'' 2>&1 | sed ':;$!{N;b};s/^\(.*\)*\(.*\)\n\2exit$/\2/p;d')"
+    else
+        echo "${PS1@P}"
+    fi
 }
 
 # Git prompt
@@ -36,4 +56,4 @@ git_prompt() {
     fi
 }
 
-export PS1="\$(make_ps1)"
+export PS1="\$(make_PS1)"
