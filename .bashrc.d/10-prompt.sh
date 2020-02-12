cdaea91e7851f348c09ee026af16cfa17dd950f3
0xC0ncord <concord@fuwafuwatime.moe>
2020-02-12 12:44:21 -0500

    Massive rewrite: use PROMPT_COMMAND functionality and rewrite ANSI escape stripping and PS1 parameter parser for Bash < 4.4

--- .bashrc.d/10-prompt.sh
+++ .bashrc.d/10-prompt.sh
@@ -3,8 +3,13 @@
 # Set GIT_DISCOVERY_ACROSS_FILESYSTEM to ensure git prompt works beyond filesystem boundaries
 export GIT_DISCOVERY_ACROSS_FILESYSTEM=1
 
+# Helper functions
+_sed_escape() {
+    echo "$1" | sed 's/[&\\/]/\\&/g;s/$/\\/;$s/\\$//;s/ /\\ /g'
+}
+
 # Git prompt
-git_prompt() {
+_git_prompt() {
     if $(which 'git' &> /dev/null); then
         TOPLEVEL="$(git rev-parse --show-toplevel 2>/dev/null)"
         if [[ -n "$TOPLEVEL" && $(basename "$TOPLEVEL" 2>/dev/null) != $(whoami) ]]; then
@@ -22,20 +27,20 @@ git_prompt() {
 }
 
 # PS1 generation
-make_PS1() {
+_make_PS1() {
     # PS1 itself
     if [[ $EUID -eq 0 ]]; then
         # Root's prompt
-	P="\[\033]0;\h:\w\007\]\[\033[01;31m\]\h\[\033[01;34m\] \w\[\033[01;33m\]\$(git_prompt) \[\033[01;34m\]\\$\[\033[00m\] "
+	    P="\[\033]0;\h:\w\007\]\[\033[01;31m\]\h\[\033[01;34m\] \w\[\033[01;33m\]\$(_git_prompt) \[\033[01;34m\]\\$\[\033[00m\] "
     else
         # User prompt
-	P="\[\033]0;\u@\h:\w\007\]\[\033[01;32m\]\u@\h\[\033[01;34m\] \w\[\033[01;33m\]\$(git_prompt) \[\033[01;34m\]\\$\[\033[00m\] "
+	    P="\[\033]0;\u@\h:\w\007\]\[\033[01;32m\]\u@\h\[\033[01;34m\] \w\[\033[01;33m\]\$(_git_prompt) \[\033[01;34m\]\\$\[\033[00m\] "
     fi
 
     # PS1 but parsed and ANSI sequences removed; for length calculation
     if [[ ${BASH_VERSION:0:1} -lt 4 ]] || [[ ${BASH_VERSION:2:1} -lt 4 ]]; then
         # @P operation is unsupported in Bash < 4.4
-        PC=$(echo "${USER}@$(hostname) $(dirs +0)$(git_prompt) $")
+        PC=$(echo ${P} | sed "s/\\\\\[\\\033.*\\\007\\\\\]//g;s/\\\\\[\\\033\\[\([0-9]\{2\};\)\?[0-9]\{2\}m\\\]//g;s/\\\u/$USER/g;s/\\\h/$(hostname)/g;s/\\\w/$(_sed_escape $(dirs +0))/g;s/\\$\x28_git_prompt)/$(_sed_escape "$(_git_prompt)")/g;s/\\\\\\$/$/g")
     else
         PC=$(echo "${P@P}" | perl -pe 's/\e([^\[\]]|\[.*?[a-zA-Z]|\].*?\a)//g' | col -b)
     fi
@@ -46,16 +51,8 @@ make_PS1() {
     else
         PS1="${P}"
     fi
-    if [[ ${BASH_VERSION:0:1} -lt 4 ]] || ([[ ${BASH_VERSION:0:1} -eq 4 ]] && [[ ${BASH_VERSION:2:1} -lt 4 ]]); then
-        # @P operation is unsupported in Bash < 4.4
-        # Nasty hack to parse the prompt - slow!
-        # FIXME: breaks when some special characters are used in commands (i.e. '!')
-        export PS1="${PS1}"
-        export -f git_prompt
-        echo "$(bash --rcfile <(echo "PS1='$PS1'") -i <<<'' 2>&1 | sed ':;$!{N;b};s/^\(.*\)*\(.*\)\n\2exit$/\2/p;d')"
-    else
-        echo "${PS1@P}"
-    fi
+    export PS1
 }
 
-export PS1="\$(make_PS1)"
+if [[ -z ${PROMPT_COMMAND} ]]; then PROMPT_COMMAND="_make_PS1"; else PROMPT_COMMAND="${PROMPT_COMMAND}; _make_PS1"; fi
+export PROMPT_COMMAND
