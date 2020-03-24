9e6b53f4ec7eead35b61f1875883f0cbb4109830
0xC0ncord <concord@fuwafuwatime.moe>
2020-03-24 17:28:32 -0400

    Create rename 02-options.sh to 01-env.sh and move rename 01-aliases.sh to 02-aliases.sh
    Also set up XDG options in 01-env.sh

--- .bashrc.d/01-env.sh
+++ .bashrc.d/01-env.sh
@@ -6,5 +6,10 @@ set -o vi                       # Enable bash VI mode
 export HISTSIZE= HISTFILESIZE=  # Infinite history
 shopt -s histappend             # Append to the history, don't overwrite it
 
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_RUNTIME_DIR="$HOME/.local/run"
+
 export PATH="$PATH:/usr/local/bin:/usr/share/bin:$HOME/.local/bin"
 export GOPATH="$HOME/.go"
