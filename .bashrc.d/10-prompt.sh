5160898ccdbc9ca0e0b76251f4be51fad589ff42
0xC0ncord <concord@fuwafuwatime.moe>
2020-06-09 13:03:05 -0400

    Use awk to generate improved bash prompt

--- .bashrc.d/10-prompt.sh
+++ .bashrc.d/10-prompt.sh
@@ -12,7 +12,7 @@ _sed_escape() {
 _selinux_prompt() {
     local CONTEXT="$(id -Z 2>/dev/null)"
     if [[ $? == 0 ]]; then
-        printf " ($(printf ${CONTEXT} | awk -F: '{print $3}'))"
+        printf "($(awk -F: '{print $3}' <<< $CONTEXT))"
     fi
 }
 
@@ -22,9 +22,9 @@ _git_prompt() {
         local TOPLEVEL="$(git rev-parse --show-toplevel 2>/dev/null)"
         if [[ -n "$TOPLEVEL" && $(basename "$TOPLEVEL" 2>/dev/null) != $(whoami) ]]; then
             if [[ -z "$(git branch)" ]]; then
-                printf " (master, empty)"
+                printf "(master, empty)"
             else
-                printf " ($(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')"
+                printf "($(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')"
                 if [[ -n "$(git diff --shortstat)" ]]; then GIT_SYMBOLS="*"; fi
                 if [[ -n "$(git ls-files ${TOPLEVEL} --exclude-standard --others)" ]]; then GIT_SYMBOLS="${GIT_SYMBOLS}%%"; fi
                 if [[ -n "${GIT_SYMBOLS}" ]]; then printf " ${GIT_SYMBOLS}"; fi
@@ -36,33 +36,65 @@ _git_prompt() {
 
 # PS1 generation
 _make_PS1() {
-    # PS1 itself
-    if [[ $EUID -eq 0 ]]; then
-        # Root's prompt
-        local P="\[\033]0;\h:\w\007\]\[\033[01;31m\]\h\[\033[01;34m\] \w\[\033[01;35m\]\$(_selinux_prompt)\[\033[01;33m\]\$(_git_prompt) \[\033[01;34m\]\\$\[\033[00m\] "
-    else
-        # User prompt
-        local P="\[\033]0;\u@\h:\w\007\]\[\033[01;32m\]\u@\h\[\033[01;34m\] \w\[\033[01;35m\]\$(_selinux_prompt)\[\033[01;33m\]\$(_git_prompt) \[\033[01;34m\]\\$\[\033[00m\] "
-    fi
-
-    # PS1 but parsed and ANSI sequences removed; for length calculation
-    if [[ ${BASH_VERSION:0:1} -lt 4 ]] || [[ ${BASH_VERSION:2:1} -lt 4 ]]; then
-        # @P operation is unsupported in Bash < 4.4
-        local PC=$(echo ${P} | sed "s/\\\\\[\\\033.*\\\007\\\\\]//g;s/\\\\\[\\\033\\[\([0-9]\{2\};\)\?[0-9]\{2\}m\\\]//g;s/\\\u/$USER/g;s/\\\h/$(hostname)/g;s/\\\w/$(_sed_escape $(dirs +0))/g;s/\\$\x28_selinux_prompt\x29\\$\x28_git_prompt\x29/$(_sed_escape "$(_selinux_prompt) $(_git_prompt)")/g;s/\\\\\\$/$/g")
-    else
-        local PC=$(echo "${P@P}" | perl -pe 's/\e([^\[\]]|\[.*?[a-zA-Z]|\].*?\a)//g' | col -b)
-    fi
-
-    # If we don't have much room for typing, use the 2-line prompt
-    # TODO truncate if still too long
     local COLS=$(tput cols)
-    if [[ ${COLS} -le $((${#PC} + 48)) ]]; then
-        PS1="\[\033[01;34m\]╭\[\033[00m\] $(echo ${P} | sed 's/\ \([0-9;\[\$]*m\\\]\)*$//')\[\033[00m\]\n\[\033[01;34m\]╰╼ \$\[\033[00m\] "
+    local TITLE="\\[\\033]0;\\u@\\h:\\w\\007\\]"
+    if [[ $EUID -eq 0 ]]; then
+        export PS1="$TITLE$(awk -v COLS=$COLS -v HOSTNAME=$HOSTNAME -v CWD="$(dirs +0)" -v SELINUX="$(_selinux_prompt)" -v GIT="$(_git_prompt)" 'BEGIN {
+            P[0]=HOSTNAME
+            P[1]=CWD
+            P[2]=SELINUX
+            P[3]=GIT
+            C[0]="\\[\\033[01;31m\\]"
+            C[1]="\\[\\033[01;34m\\]"
+            C[2]="\\[\\033[01;35m\\]"
+            C[3]="\\[\\033[01;33m\\]"
+            for (i in P) {
+                if (length(P[i]) != 0) {
+                    R=R" "i""P[i]" "
+                }
+            }
+            L=length(R)
+            if (COLS + 3 < L) {
+                R=substr(R,1,COLS - 1)"…"
+            }
+            for (i in P) {
+                gsub(" "i, C[i], R)
+            }
+            if (COLS <= L + 48) {
+                R=C[1]"╭ "R"\n"C[1]"╰╼ "
+            }
+            R=R""C[1]"$"
+            printf R"\\[\\033[00m\\] "
+        }')"
     else
-        PS1="${P}"
+        export PS1="$TITLE$(awk -v COLS=$COLS -v USER=$USER -v HOSTNAME=$HOSTNAME -v CWD="$(dirs +0)" -v SELINUX="$(_selinux_prompt)" -v GIT="$(_git_prompt)" 'BEGIN {
+            P[0]=USER"@"HOSTNAME
+            P[1]=CWD
+            P[2]=SELINUX
+            P[3]=GIT
+            C[0]="\\[\\033[01;32m\\]"
+            C[1]="\\[\\033[01;34m\\]"
+            C[2]="\\[\\033[01;35m\\]"
+            C[3]="\\[\\033[01;33m\\]"
+            for (i in P) {
+                if (length(P[i]) != 0) {
+                    R=R" "i""P[i]" "
+                }
+            }
+            L=length(R)
+            if (COLS + 3 < L) {
+                R=substr(R,1,COLS - 1)"…"
+            }
+            for (i in P) {
+                gsub(" "i, C[i], R)
+            }
+            if (COLS <= L + 48) {
+                R=C[1]"╭ "R"\n"C[1]"╰╼ "
+            }
+            R=R""C[1]"$"
+            printf R"\\[\\033[00m\\] "
+        }')"
     fi
-
-    export PS1
 }
 
 if [[ -z ${PROMPT_COMMAND} ]]; then
