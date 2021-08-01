e12d3137bbee3b5a174aea777d8e9ccd95a15c3c
0xC0ncord <me@concord.sh>
2021-08-01 11:43:31 -0400

    aliases: no more checking highlight version

--- .bashrc.d/02-aliases.sh
+++ .bashrc.d/02-aliases.sh
@@ -20,12 +20,8 @@ function main {
 
     # The below aliases require additional dependencies
     if command -v highlight &>/dev/null; then
-        VER="$(highlight --version | sed -n '/version/s/[^0-9]//gp')"
-        if [[ $VER -ge 335 ]]; then
-            # --stdout option only available in 3.35 and onwards; too breaking to use without it, so don't alias if it's not available
-            alias ccat='highlight --out-format=ansi --stdout --force'
-            alias cat='ccat'
-        fi
+        alias ccat='highlight --out-format=ansi --stdout --force'
+        alias cat='ccat'
     fi
     if command -v src-hilite-lesspipe.sh &>/dev/null; then
         export LESSOPEN="|/usr/bin/src-hilite-lesspipe.sh %s"
