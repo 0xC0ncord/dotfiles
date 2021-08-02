b8474612164f0e89921195f2c05c80be774eee35
0xC0ncord <me@concord.sh>
2021-08-02 18:24:08 -0400

    env: histappend is too annoying with multiple shells

--- .bashrc.d/01-env.sh
+++ .bashrc.d/01-env.sh
@@ -11,7 +11,6 @@ function main {
     shopt -s autocd                                             # Allows cding into a directory by just typing its name
     set -o vi                                                   # Enable bash VI mode
     export HISTSIZE= HISTFILESIZE=                              # Infinite history
-    shopt -s histappend                                         # Append to the history, don't overwrite it
 
     _append_to_path /usr/local/bin
     _append_to_path /usr/share/bin
