4675012ca22b625fe9383e43d718287874047358
0xC0ncord <me@concord.sh>
2022-04-25 17:07:25 -0400

    env: rework and add additional XDG env vars

--- .bashrc.d/01-env.sh
+++ .bashrc.d/01-env.sh
@@ -19,14 +19,26 @@ function main {
     export GOPATH="$HOME/.go"
 
     # Various options to force applications to use XDG and avoid $HOME clutter
-    export XDG_CONFIG_HOME="$HOME/.config"
-    export XDG_CACHE_HOME="$HOME/.cache"
-    export XDG_DATA_HOME="$HOME/.local/share"
-    if [[ -d /run/user/$UID ]]; then
-        export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-/run/user/$UID}"
-    else
-        export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-$HOME/.local/run}"
+    export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+    export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
+    export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
+    export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+    if [[ -z "$XDG_RUNTIME_DIR" ]]; then
+        if [[ -d /run/user/$UID ]]; then
+            export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-/run/user/$UID}"
+        else
+            export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-$HOME/.local/run}"
+        fi
     fi
+    export XDG_DESKTOP_DIR="${XDG_DESKTOP_DIR:-$HOME/Desktop}"
+    export XDG_DOCUMENTS_DIR="${XDG_DOCUMENTS_DIR:-$HOME/Documents}"
+    export XDG_DOWNLOAD_DIR="${XDG_DOWNLOAD_DIR:-$HOME/Downloads}"
+    export XDG_MUSIC_DIR="${XDG_MUSIC_DIR:-$HOME/Music}"
+    export XDG_PICTURES_DIR="${XDG_PICTURES_DIR:-$HOME/Pictures}"
+    export XDG_PUBLICSHARE_DIR="${XDG_PUBLICSHARE_DIR:-$HOME/Public}"
+    export XDG_SCREENSHOTS_DIR="${XDG_SCREENSHOTS_DIR:-$XDG_PICTURES_DIR/Screenshots}"
+    export XDG_TEMPLATES_DIR="${XDG_TEMPLATES_DIR:-$HOME/Templates}"
+    export XDG_VIDEOS_DIR="${XDG_VIDEOS_DIR:-$HOME/Videos}"
 
     export PYTHONSTARTUP="$XDG_CONFIG_HOME"/python/pythonrc
     export GTK_RC_FILES="$XDG_CONFIG_HOME"/gtk-1.0/gtkrc
